428ec73bd501595d362eb7c7c43ded7e24db1c0e
AntonioAlmeidaRego <antonio.alm_pdf@hotmail.com>
2018-12-09 13:28:05 -0300

    Adicionado os serviços

---
+++ Urna Client/app/controllers/UrnaClient.java
@@ -0,0 +1,33 @@
+package controllers;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+
+import models.Candidato;
+import models.CandidatoNaoExist;
+import play.libs.WS;
+import play.libs.WS.HttpResponse;
+import play.mvc.Controller;
+
+public class UrnaClient extends Controller{
+	private static final Gson g = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().setPrettyPrinting().create();
+	
+	public static void main() {
+		render();
+	}
+	
+	public static void pegarCandidato(int num) {
+		HttpResponse response = WS.url(""+num).get();
+		if(response.success()) {
+			JsonElement json = response.getJson();
+			Candidato candidato = g.fromJson(json, Candidato.class);
+            renderJSON(candidato);
+		}else {
+			CandidatoNaoExist candidatoNaoExist = new CandidatoNaoExist();
+			candidatoNaoExist.string = "candidatoNaoExiste";
+			renderJSON(candidatoNaoExist);
+		}
+	}
+ 
+}
